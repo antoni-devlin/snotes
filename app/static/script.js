14c5350a69e35491569da2a2d47304077a9de3bd
antoni-devlin <antoni.devlin@gmail.com>
2023-02-13 17:56:48 +0000

    Remove global script.js and hardcoded link in app.html

--- app/static/script.js
+++
@@ -1,6 +0,0 @@
-document
-  .getElementsByClassName("toggle-menu")[0]
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-    document.getElementsByClassName("sidebar")[0].classList.toggle("toggled");
-  });
